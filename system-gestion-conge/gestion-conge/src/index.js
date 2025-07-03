a4ea7da05bd61f4607a0bc332a5a5ff7375d498e
hibarebai <160390968+hibarebai@users.noreply.github.com>
2025-07-03 16:07:53 +0100

    frontend1

--- system-gestion-conge/gestion-conge/src/index.js
+++ system-gestion-conge/gestion-conge/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Authentification from './Authentification';
+import Inscription from './Inscription';
+import HomeEmploye from './HomeEmploye';
+import HomeDRH from './HomeDRH';
+import Layout from './Layout';
+import Home from './Home';
+import SoldeConge from './gestion-conge/src/SoldeConge';
+import ListeCongés from './gestion-conge/src/ListeCongés';
+import ModifierDemande from './gestion-conge/src/ModifierDemande';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <BrowserRouter>
+  <Routes>
+    <Route path="/authentification" element={<Authentification></Authentification>} ></Route>  
+    <Route path="/inscription" element={<Inscription></Inscription>} ></Route>  
+    <Route path="/" element={<Layout></Layout>} ></Route>    
+    <Route path="/homeemploye" element={<HomeEmploye></HomeEmploye>} ></Route>    
+    <Route path="/homedrh" element={<HomeDRH></HomeDRH>} ></Route>    
+    <Route path="/home" element={<Home></Home>} ></Route>    
+    <Route path="/soldeconge" element={<SoldeConge></SoldeConge>} ></Route>    
+    <Route path="/listeconges" element={<ListeCongés></ListeCongés>} ></Route>    
+    <Route path="/modifier" element={<ModifierDemande />} />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </Routes> 
+
+
+</BrowserRouter>
+);
