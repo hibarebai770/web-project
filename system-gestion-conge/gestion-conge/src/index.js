54029e84c41a53cbd1565fffc1ddf3dd36171aac
hibarebai <160390968+hibarebai@users.noreply.github.com>
2025-07-04 15:50:21 +0100

    index.js

---
+++ system-gestion-conge/gestion-conge/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Authentification from './Authentification';
+import Inscription from './Inscription';
+import HomeEmploye from './HomeEmploye';
+import HomeDRH from './HomeDRH';
+import Layout from './Layout';
+import Home from './Home';
+import SoldeConge from './SoldeConge';
+import ListeCongés from './ListeCongés';
+import ModifierDemande from './ModifierDemande';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  <Routes>
+    <Route path="/authentification" element={<Authentification></Authentification>} ></Route>  
+    <Route path="/inscription" element={<Inscription></Inscription>} ></Route>  
+    <Route path="/" element={<Layout></Layout>} ></Route>    
+    <Route path="/homeemploye" element={<HomeEmploye></HomeEmploye>} ></Route>    
+    <Route path="/homedrh" element={<HomeDRH></HomeDRH>} ></Route>    
+    <Route path="/home" element={<Home></Home>} ></Route>    
+    <Route path="/soldeconge" element={<SoldeConge></SoldeConge>} ></Route>    
+    <Route path="/listeconges" element={<ListeCongés></ListeCongés>} ></Route>    
+    <Route path="/modifier" element={<ModifierDemande />} />
+
+  </Routes> 
+
+
+</BrowserRouter>
+);
